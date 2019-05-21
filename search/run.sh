a80dbdac3124772d0147e1090a0f62b0a2ff3ba8
Jomi F. Hubner <jomifred@gmail.com>
2019-05-21 15:04:56 -0300

    run benchmark in iMac and jason 2.4

--- search/run.sh
+++ search/run.sh
@@ -1,5 +1,4 @@
+#time java -cp ../libs/jason-2.3.jar jason.infra.centralised.RunCentralisedMAS search.mas2j
 
-#java -Xmx4g  -Dprofile.properties=../jip.properties  -javaagent:../jip-1.2/profile/profile.jar  -cp ../../jason-svn/lib/jason.jar:bin/classes jason.infra.centralised.RunCentralisedMAS search.mas2j
-#time java -cp ~/bin/Jason-1.4.2/lib/jason.jar:bin/classes jason.infra.centralised.RunCentralisedMAS search.mas2j
 source $JASON_HOME/scripts/jason-setup
 time java -cp $JASON_JAR:bin/classes jason.infra.centralised.RunCentralisedMAS search.mas2j
--- st-claus/run.sh
+++ st-claus/run.sh
@@ -1,4 +1,4 @@
-#time java -cp ../jason-agere13.jar:bin/classes jason.infra.centralised.RunCentralisedMAS jason_stclaus.mas2j
-#time java -cp ~/bin/Jason-1.4.2/lib/jason.jar:bin/classes jason.infra.centralised.RunCentralisedMAS jason_stclaus.mas2j
+#time java -cp ../libs/jason-2.3.jar:bin/classes jason.infra.centralised.RunCentralisedMAS jason_stclaus.mas2j 2> x~
+
 source $JASON_HOME/scripts/jason-setup
 time java -cp $JASON_JAR:bin/classes jason.infra.centralised.RunCentralisedMAS jason_stclaus.mas2j 2> x~
